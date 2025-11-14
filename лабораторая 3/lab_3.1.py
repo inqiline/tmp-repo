8c0c7fb615317697120113ba24ed555cb4dcda55
igor_Harizov <122484484+inqiline@users.noreply.github.com>
2025-11-14 19:57:48 +0300

    Add files via upload

---
+++ лабораторая 3/lab_3.1.py
@@ -0,0 +1,17 @@
+# TODO Напишите функцию для поиска индекса товара
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+def find_index(item, items):
+    try:
+        return items.index(item)
+    except ValueError:
+        return None
+
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_index(find_item, items_list)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
